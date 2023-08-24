713b33a1011a7900d7edf10021d790f33dd3a2ab
Fl0yk <kosach.dmitriy@mail.ru>
2023-08-24 21:15:28 +0300

    lab 2 end min

---
+++ WEB_153501_Kosach/WEB_153501_Kosach/Models/ListDemo.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_153501_Kosach.Models
+{
+    public class ListDemo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ WEB_153501_Kosach/WEB_153501_Kosach/Views/Shared/Components/CartViewComponet.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153501_Kosach.Components
+{
+    public class Cart: ViewComponent
+    {
+        private decimal balance;
+        private int countOfProducts;
+
+        public async Task<IViewComponentResult> InvokeAsync(string nameOfData) => nameOfData.ToLower() switch
+        {
+            "balance" => Content(balance.ToString("F2")),
+            "count" => Content(countOfProducts.ToString()),
+            _ => throw new NotImplementedException()
+        };
+    }
+}
